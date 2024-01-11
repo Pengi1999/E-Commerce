7cb61996c0dc6af10a19b06d538b5a784d8d2037
Nhat Hao <doannhathao2015@gmail.com>
2024-01-11 15:47:43 +0700

    Done giao dien Profile

--- app/src/main/java/com/nhathao/e_commerce/activities/MainActivity.kt
+++ app/src/main/java/com/nhathao/e_commerce/activities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.nhathao.e_commerce.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.nhathao.e_commerce.R
 import com.nhathao.e_commerce.databinding.ActivityMainBinding
+import com.nhathao.e_commerce.fragments.HomeFragment
+import com.nhathao.e_commerce.fragments.ProfileFragment
 
 private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_layout, fragment)
+            commit()
+        }
     }
 }
