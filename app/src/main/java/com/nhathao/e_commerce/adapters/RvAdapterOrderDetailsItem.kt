d083b3edd44233b871d4c0600ce39fdb83d22cdb
Nhat Hao <doannhathao2015@gmail.com>
2024-03-01 14:50:10 +0700

    Done Hien thi du lieu OrderDetail len giao dien

---
+++ app/src/main/java/com/nhathao/e_commerce/adapters/RvAdapterOrderDetailsItem.kt
@@ -0,0 +1,97 @@
+package com.nhathao.e_commerce.adapters
+
+import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
+import android.graphics.Paint
+import android.util.Base64
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.nhathao.e_commerce.R
+import com.nhathao.e_commerce.models.OrderDetail
+import com.nhathao.e_commerce.models.Product
+import com.nhathao.e_commerce.models.Quantity
+
+class RvAdapterOrderDetailsItem (private var ds:List<OrderDetail>): RecyclerView.Adapter<RvAdapterOrderDetailsItem.OrderDetailsItemViewHolder>() {
+    private val dbRefBag: DatabaseReference = FirebaseDatabase.getInstance().getReference("Bags")
+    private val dbRefProduct: DatabaseReference = FirebaseDatabase.getInstance().getReference("Products")
+    private val dbRefQuantity: DatabaseReference = FirebaseDatabase.getInstance().getReference("Quantities")
+    private lateinit var quantity: Quantity
+    private lateinit var product: Product
+
+    class OrderDetailsItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderDetailsItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_orderdetails_item, parent, false)
+        return OrderDetailsItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return ds.size
+    }
+
+    @SuppressLint("ResourceAsColor", "SetTextI18n")
+    override fun onBindViewHolder(holder: OrderDetailsItemViewHolder, position: Int) {
+        val imgProduct = holder.itemView.findViewById<ImageView>(R.id.imgProduct)
+        val txtProductName = holder.itemView.findViewById<TextView>(R.id.txtProductName)
+        val txtProductDes = holder.itemView.findViewById<TextView>(R.id.txtProductDes)
+        val txtSelectedColor = holder.itemView.findViewById<TextView>(R.id.txtSelectedColor)
+        val txtSelectedSize = holder.itemView.findViewById<TextView>(R.id.txtSelectedSize)
+        val txtUnitsValue = holder.itemView.findViewById<TextView>(R.id.txtUnitsValue)
+        val txtProductPrice = holder.itemView.findViewById<TextView>(R.id.txtProductPrice)
+        val txtProductPriceSale = holder.itemView.findViewById<TextView>(R.id.txtProductPriceSale)
+
+        holder.itemView.apply {
+            dbRefQuantity.child(ds[position].quantityId).get().addOnSuccessListener { it ->
+                if (it.exists()){
+                    quantity = Quantity(
+                        it.child("quantityId").value.toString(),
+                        it.child("productId").value.toString(),
+                        it.child("productColor").value.toString(),
+                        it.child("productSize").value.toString(),
+                        it.child("quantity").value.toString().toInt()
+                    )
+                    txtSelectedColor.text = quantity.productColor
+                    txtSelectedSize.text = quantity.productSize
+                    dbRefProduct.child(quantity.productId).get().addOnSuccessListener {
+                        if (it.exists()){
+                            product = Product(
+                                it.child("productId").value.toString(),
+                                it.child("productName").value.toString(),
+                                it.child("productDescribe").value.toString(),
+                                it.child("productImage").value.toString(),
+                                it.child("productSex").value.toString(),
+                                it.child("productType").value.toString(),
+                                it.child("productCategory").value.toString(),
+                                it.child("productBrand").value.toString(),
+                                it.child("productMode").value.toString(),
+                                it.child("productPrice").value.toString().toInt(),
+                                it.child("productRating").value.toString().toFloat(),
+                                it.child("productRatingQuantity").value.toString().toInt()
+                            )
+                            val bytes = Base64.decode(product.productImage, Base64.DEFAULT)
+                            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                            imgProduct.setImageBitmap(bitmap)
+                            txtProductName.text = product.productName
+                            txtProductDes.text = product.productDescribe
+                            txtProductPrice.text = "${product.productPrice}$"
+                            if (product.productMode != "" && product.productMode != "NEW"){
+                                val saleValue = product.productMode.substring(1,3).toFloat() / 100
+                                txtProductPrice.setTextColor(R.color.text_color_hint)
+                                txtProductPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                                txtProductPriceSale.text = "${product.productPrice - (product.productPrice * saleValue).toInt()}$"
+                            }
+                        }
+                    }
+                }
+            }
+            txtUnitsValue.text = ds[position].quantity.toString()
+        }
+    }
+}
--- app/src/main/java/com/nhathao/e_commerce/fragments/OrderDetailsFragment.kt
+++ app/src/main/java/com/nhathao/e_commerce/fragments/OrderDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.nhathao.e_commerce.fragments
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,9 +11,17 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.nhathao.e_commerce.R
+import com.nhathao.e_commerce.adapters.RvAdapterBagItem
+import com.nhathao.e_commerce.adapters.RvAdapterOrderDetailsItem
+import com.nhathao.e_commerce.models.Bag
 import com.nhathao.e_commerce.models.Order
+import com.nhathao.e_commerce.models.OrderDetail
+import com.nhathao.e_commerce.models.ShippingAddress
 import com.nhathao.e_commerce.models.User
 import java.text.SimpleDateFormat
 
@@ -29,6 +38,8 @@ private const val ARG_PARAM2 = "param2"
 @SuppressLint("SimpleDateFormat")
 class OrderDetailsFragment : Fragment() {
     private lateinit var dbRefOrder: DatabaseReference
+    private lateinit var dbRefOrderDetail: DatabaseReference
+    private lateinit var dbRefShippingAddress: DatabaseReference
     private lateinit var btnBack: ImageButton
     private lateinit var btnSearch: ImageButton
     private lateinit var btnReorder: Button
@@ -37,6 +48,13 @@ class OrderDetailsFragment : Fragment() {
     private lateinit var txtCreateTime: TextView
     private lateinit var txtItemQuantity: TextView
     private lateinit var txtOrderStatus: TextView
+    private lateinit var txtShippingAddress: TextView
+    private lateinit var txtPaymentMethod: TextView
+    private lateinit var txtDeliveryMethod: TextView
+    private lateinit var txtDiscount: TextView
+    private lateinit var txtTotalAmount: TextView
+    private lateinit var rcvOrderDetail: RecyclerView
+    private lateinit var dsOrderDetails: MutableList<OrderDetail>
     private lateinit var selectedOrder: Order
     private val dateFormat = SimpleDateFormat("dd-MM-yyyy")
     // TODO: Rename and change types of parameters
@@ -59,6 +77,9 @@ class OrderDetailsFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_order_details, container, false)
 
+        dbRefShippingAddress = FirebaseDatabase.getInstance().getReference("ShippingAddresses")
+        dbRefOrderDetail = FirebaseDatabase.getInstance().getReference("OrderDetails")
+
         btnBack = view.findViewById(R.id.btnBack)
         btnSearch = view.findViewById(R.id.btnSearch)
         btnReorder = view.findViewById(R.id.btnReorder)
@@ -67,12 +88,88 @@ class OrderDetailsFragment : Fragment() {
         txtCreateTime = view.findViewById(R.id.txtCreateTime)
         txtItemQuantity = view.findViewById(R.id.txtItemQuantity)
         txtOrderStatus = view.findViewById(R.id.txtOrderStatus)
+        txtShippingAddress = view.findViewById(R.id.txtShippingAddress)
+        txtPaymentMethod = view.findViewById(R.id.txtPaymentMethod)
+        txtDeliveryMethod = view.findViewById(R.id.txtDeliveryMethod)
+        txtDiscount = view.findViewById(R.id.txtDiscount)
+        txtTotalAmount = view.findViewById(R.id.txtTotalAmount)
+        rcvOrderDetail = view.findViewById(R.id.rcvOrderDetail)
 
         selectedOrder = this.activity?.intent?.getSerializableExtra("selectedOrder") as Order
 
         txtOrderId.text = "Order ${selectedOrder.orderId}"
         txtCreateTime.text = dateFormat.format(selectedOrder.createTime)
+        txtOrderStatus.text = selectedOrder.status
 
+        if (selectedOrder.status == "Delivered")
+            txtOrderStatus.setTextColor(Color.parseColor("#2AA952"))
+        else if (selectedOrder.status == "Cancelled")
+            txtOrderStatus.setTextColor(resources.getColor(R.color.text_color_hint))
+        else
+            txtOrderStatus.setTextColor(resources.getColor(R.color.red_button))
+
+        dsOrderDetails = mutableListOf()
+
+        dbRefOrderDetail.get().addOnSuccessListener {
+            if (it.exists()){
+                var quantityValue = 0
+                dsOrderDetails.clear()
+                for (orderDetailSnap in it.children) {
+                    if (orderDetailSnap.child("orderId").value.toString() == selectedOrder.orderId) {
+                        quantityValue += orderDetailSnap.child("quantity").value.toString().toInt()
+                        val orderDetail = OrderDetail(
+                            orderDetailSnap.child("orderDetailId").value.toString(),
+                            orderDetailSnap.child("orderId").value.toString(),
+                            orderDetailSnap.child("quantityId").value.toString(),
+                            orderDetailSnap.child("quantity").value.toString().toInt()
+                        )
+                        dsOrderDetails.add(orderDetail)
+                    }
+                }
+                txtItemQuantity.text = "$quantityValue items"
+                val orderDetailAdapter = RvAdapterOrderDetailsItem(dsOrderDetails)
+                rcvOrderDetail.adapter = orderDetailAdapter
+                rcvOrderDetail.layoutManager = LinearLayoutManager(
+                    this.requireContext(),
+                    LinearLayoutManager.VERTICAL,
+                    false
+                )
+            }
+        }
+
+        dbRefShippingAddress.child(selectedOrder.shippingAddressId).get().addOnSuccessListener {
+            if (it.exists()){
+                val shippingAddress = ShippingAddress(
+                    it.child("shippingAddressId").value.toString(),
+                    it.child("userAccountName").value.toString(),
+                    it.child("fullName").value.toString(),
+                    it.child("address").value.toString(),
+                    it.child("city").value.toString(),
+                    it.child("region").value.toString(),
+                    it.child("zipCode").value.toString(),
+                    it.child("country").value.toString(),
+                    it.child("used").value.toString().toBoolean(),
+                    it.child("status").value.toString().toBoolean(),
+                )
+                txtShippingAddress.text = "${shippingAddress.address}, ${shippingAddress.city}, ${shippingAddress.region} ${shippingAddress.zipCode}, ${shippingAddress.country}"
+            }
+        }
+
+        txtPaymentMethod.text = selectedOrder.paymentMethod
+
+        when (selectedOrder.deliveryMethod) {
+            "FedEx" -> txtDeliveryMethod.text = "${selectedOrder.deliveryMethod}, 3 days, 15$"
+            "USPS" -> txtDeliveryMethod.text = "${selectedOrder.deliveryMethod}, 3 days, 16$"
+            "DHL" -> txtDeliveryMethod.text = "${selectedOrder.deliveryMethod}, 2 days, 17$"
+        }
+
+        when (selectedOrder.promoCode) {
+            "personalpromocode10" -> txtDiscount.text = "10%, Personal promo code"
+            "summer2024" -> txtDiscount.text = "15%, Summer Sale promo code"
+            "personalpromocode22" -> txtDiscount.text = "22%, Personal promo code"
+        }
+
+        txtTotalAmount.text = "${selectedOrder.totalPrice}$"
 
         btnBack.setOnClickListener {
             this.activity?.supportFragmentManager?.popBackStack()
